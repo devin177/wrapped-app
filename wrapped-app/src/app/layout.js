59b6817a896df62cfcbc0358525f5419c1a0319f
devin177 <36557400+devin177@users.noreply.github.com>
2025-02-01 02:14:47 +0000

    analytics

--- wrapped-app/src/app/layout.js
+++ wrapped-app/src/app/layout.js
@@ -1,4 +1,5 @@
 import localFont from 'next/font/local'
+import { Analytics } from '@vercel/analytics/next';
 
 const gotham = localFont({
   src: [
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={gotham.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
